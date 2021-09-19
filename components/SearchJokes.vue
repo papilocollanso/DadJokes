6d742022bed248f38bf8bddcd8bff9d55a573be9
prosper collins <prospercollins19@gmail.com>
2021-09-19 09:34:07 +0100

    first commit

---
+++ components/SearchJokes.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+<form @submit.prevent="onSubmit">
+<input type="text" v-model="text" placeholder="Search Jokes..."/>
+<input type="submit" value="Search Jokes"/>
+</form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'searchJokes',
+    data(){
+      return{
+         text:''
+      }
+    },
+    methods:{
+      onSubmit(){
+this.$emit('search-text',this.text);
+this.text="";
+      }
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+ <h2>Welcome to the best corny dad jokes on the web</h2>
+</template>
+
+<script>
+ export default {
+        head(){
+            return {
+                title: "Welcome To Dad Jokes",
+                meta: [
+                    {
+                    hid:"description",
+                    name: "description",
+                    content: "Best place for corny dad jokes"
+                }]
+            }
+        }
+
+    };
+</script>
